d76a5f60d0b1a37fa0d1bfbdd7bdc30e51e3205f
nikola <nikolastojkovic05@gmail.com>
2023-01-18 14:25:02 +0100

    drugi commit

---
+++ WebShop/Controllers/WebShopController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebShopController : ControllerBase
+    {
+      
+        private readonly ILogger<WebShopController> _logger;
+
+        public WebShopController(ILogger<WebShopController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Version()
+        {
+            return "Web shop is working";
+        }
+    }
+}
